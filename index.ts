6e84e1a114243631ee8ff7c0e0c4ea455b0f682b
Keshav Ashta <keshavashta16@gmail.com>
2022-08-08 18:52:46 +0530

    done soemtthing

--- index.ts
+++ index.ts
@@ -373,27 +373,26 @@ async function __sync_new_fields(eventFields: TableField[], global: any, config:
     }
 }
 
-const __flatten_object = (props, sep = '__', nestedChain = []) => {
-    let newProps = {}
-    for (const [key, value] of Object.entries(props)) {
-        if (key === '$elements' || key === '$groups' || key === '$active_feature_flags') {
-            // Hide 'internal' properties used in event processing
-        } else if (key === '$set') {
-            newProps = { ...newProps, $set: { ...props[key], ...__flatten_object(props[key], sep) } }
-        } else if (key === '$set_once') {
-            newProps = { ...newProps, $set_once: { ...props[key], ...__flatten_object(props[key], sep) } }
-        } else if (Array.isArray(value)) {
-            newProps = { ...newProps, ...__flatten_object(props[key], sep, [...nestedChain, key]) }
-        } else if (value !== null && typeof value === 'object' && Object.keys(value).length > 0) {
-            newProps = { ...newProps, ...__flatten_object(props[key], sep, [...nestedChain, key]) }
-        } else {
-            if (nestedChain.length > 0) {
-                newProps[nestedChain.join(sep) + `${sep}${key}`] = value
+function __flatten_object(obj: any) {
+    let result: any = {}
+    for (const i in obj) {
+        if (!obj.hasOwnProperty(i)) continue
+        if (Array.isArray(obj[i])) {
+            obj[i].forEach((value, index) => {
+                const temp = __flatten_object(value)
+                for (const j in temp) {
+                    result[i + '__' + (index + '__' + j).replace(/\$/g, '')] = Array.isArray(temp[j]) ? JSON.stringify(temp[j]) : temp[j]
+                }
+            })
+
+        } else if ((typeof obj[i]) === 'object' && !Array.isArray(obj[i])) {
+            const temp = __flatten_object(obj[i])
+            for (const j in temp) {
+                result[(i + '__' + j).replace(/\$/g, '')] = Array.isArray(temp[j]) ? JSON.stringify(temp[j]) : temp[j]
             }
+        } else {
+            result[i.replace(/\$/, '')] = Array.isArray(obj[i]) ? JSON.stringify(obj[i]) : obj[i]
         }
     }
-    if (nestedChain.length > 0) {
-        return { ...newProps }
-    }
-    return { ...props, ...newProps }
+    return result
 }
